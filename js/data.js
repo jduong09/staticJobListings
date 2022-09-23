acc9c62b2d7b703c3c490c999fa090e753011aed
Justin Duong <jduong@umich.edu>
2022-09-22 21:09:41 -0400

    Initialized repo. Added html structure and css for each job listing. Began javascript.

---
+++ js/data.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const http = new XMLHttpRequest();
+  http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      const jsonObj = JSON.parse(this.responseText);
+      console.log(jsonObj);
+    }
+  }
+
+  http.open('GET', '../data.json', true);
+  http.send();
+});
