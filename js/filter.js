bb17aa5f4e7aa9c16713eca2f2225375c638732b
Justin Duong <jduong@umich.edu>
2022-09-27 19:58:08 -0400

    Added click listeners on filter items in each job listing in order to sort jobs by a certain filter.

--- js/filter.js
+++ js/filter.js
@@ -3,9 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
   // When hovering, add active states.
   // When button is clicked, if there is no filter before the click, then we need a new box that will display abovve the list of jobs, and it will 
   // Create item that shows that is being filter, and then filter the list by that item.
+  const main = document.querySelector('main');
   const filterItems = document.querySelectorAll('.list-filters > li');
   const filteredItemDiv = document.getElementById('div-filtered-items');
   const filteredItemsList = document.querySelector('.list-filtered-items');
+  const listOfJobs = document.querySelector('.list-jobs');
+  const listJobs = document.getElementsByClassName('listing-job');
+  const clearBtn = document.getElementById('btn-clear');
+  let filteredJobs = [];
+
+  clearBtn.addEventListener('click', () => {
+    filteredItemsList.innerHTML = '';
+    filteredItemDiv.classList.add('hide');
+    
+    for (let i = 0; i < listOfJobs.children.length; i++) {
+      listOfJobs.children[i].classList.remove('hide');
+    }
+    filteredJobs = [];
+  });
   
   for (let i = 0; i < filterItems.length; i++) {
     const filterItem = filterItems[i];
@@ -15,13 +30,14 @@ document.addEventListener('DOMContentLoaded', () => {
       if (filteredItemDiv.classList.contains('hide')) {
         filteredItemDiv.classList.remove('hide');
       }
+
       // Need to check if this filtered click has already been added to the filtered list.
-      if (filteredItemsList.length > 1) {
-        for (let j = 0; j < filteredItemsList.length; j++) {
-          const currentFilteredItem = filteredItemsList[j];
-  
-          if (currentFilteredItem.innerHTML === e.target.innerHTML) {
+      if (filteredItemsList.children.length > 1) {
+        for (let j = 0; j < filteredItemsList.children.length; j++) {
+          const currentFilteredItem = filteredItemsList.children[j];
+          if (currentFilteredItem.textContent === e.target.innerHTML) {
             // If filtered list contains clicked filter, do nothing and end function.
+            console.log('Already clicked this filter!');
             return false;
           }
         }
@@ -30,7 +46,39 @@ document.addEventListener('DOMContentLoaded', () => {
       const newFilteredItem = document.createElement('li');
       newFilteredItem.classList.add(`filter-${e.target.innerHTML.toLowerCase()}`);
       newFilteredItem.innerHTML = e.target.innerHTML;
+
+      const btnDeleteFilter = document.createElement('button');
+      btnDeleteFilter.classList.add('btn-delete-filter');
+      const imgDelete = document.createElement('img');
+      imgDelete.classList.add('img-delete-filter');
+      imgDelete.src = './images/icon-remove.svg';
+      imgDelete.alt = 'Remove icon for deleting filter';
+      btnDeleteFilter.append(imgDelete);
+      newFilteredItem.append(btnDeleteFilter);
       filteredItemsList.append(newFilteredItem);
+
+      imgDelete.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(e.target.parentElement.parentElement.textContent);
+      });
+
+      // Need logic for actual filtering
+      for (let k = 0; k < listJobs.length; k++) {
+        const listJob = listJobs[k];
+        listJob.classList.add('hide');
+        const currentListFilters = listJob.children[2];
+        for (let l = 0; l < currentListFilters.children.length; l++) {
+          const currentFilter = currentListFilters.children[l];
+
+          if (currentFilter.innerHTML === e.target.innerHTML) {
+            filteredJobs.push(listJob);
+            break;
+          }
+        }
+      }
+      filteredJobs.forEach(filteredJob => {
+        filteredJob.classList.remove('hide');
+      });
     });
   }
 })
