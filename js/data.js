15c751238442520957e17d01cbbf9d45fde74aa1
Justin Duong <jduong@umich.edu>
2022-09-28 17:03:42 -0400

    Added more await statements to catch why loaded data does not have work.

--- js/data.js
+++ js/data.js
@@ -86,11 +86,11 @@ function addNewJobListing(jsonObj, listJobs) {
 document.addEventListener('DOMContentLoaded', async () => {
   const mainListOfJobs = document.querySelector('.list-jobs');
   const http = new XMLHttpRequest();
-  http.onreadystatechange = function() {
+  http.onreadystatechange = async () => {
     if (this.readyState == 4 && this.status == 200) {
       const jsonObj = JSON.parse(this.responseText);
       
-      addNewJobListing(jsonObj, mainListOfJobs);
+      await addNewJobListing(jsonObj, mainListOfJobs);
     }
   }
 
@@ -98,7 +98,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   await http.send();
 
 
-  const filterItems = document.querySelectorAll('.list-filters > li');
+  const filterItems = await document.querySelectorAll('.list-filters > li');
   console.log(filterItems);
   const filteredItemDiv = document.getElementById('div-filtered-items');
   const filteredItemsList = document.querySelector('.list-filtered-items');
