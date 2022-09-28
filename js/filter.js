e64a9bf30877535d7e9cf4686664cc60198dfecf
Justin Duong <jduong@umich.edu>
2022-09-28 16:32:29 -0400

    Changed load time on filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('load', () => {
   // Make clickable buttons on each of the filter list items in the list of filters.
   // When hovering, add active states.
   // When button is clicked, if there is no filter before the click, then we need a new box that will display abovve the list of jobs, and it will 
